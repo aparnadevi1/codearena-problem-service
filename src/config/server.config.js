d5b39c95c022aab3db3a7043d6c53d4b71b155c3
Aparna <chodapuneediaparnadevi@gmail.com>
2025-09-25 19:07:17 +0530

    Feat:Added mongoose db config

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,5 +1,7 @@
 const dotenv=require("dotenv");
 dotenv.config();
 module.exports={
-    PORT:process.env.PORT||3143
+    PORT:process.env.PORT||3143,
+    ATLAS_DB_URL:process.env.ATLAS_DB_URL,
+    NODE_ENV:process.env.NODE_ENV
 }
